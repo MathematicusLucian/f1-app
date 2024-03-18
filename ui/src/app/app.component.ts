86af5d1aead4cfe6bd0af798e73e2c7d4a39951a
k1n4kut4 <recruitlukejones@gmail.com>
2024-03-18 17:33:38 +0000

    UI call to API; fetched circuit data

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { map } from 'rxjs/operators';
 import {AppService} from './app.service';
 
 @Component({
@@ -7,13 +10,27 @@ import {AppService} from './app.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'App!!';
-  msg: string;
+  title = 'F1 stats';
+  track:any={}
+	seasonRounds=[
+		{"id_track":1, "name":"Monaco"},
+		{"id_track":2, "name":"Silverstone"}
+	]
+  BASE_URL = "http://localhost/api/round_results?season=2008&round_no=6"
+  seasonRoundData = {};
+  constructor(private appService: AppService, private http: HttpClient) { }
 
-  constructor(private appService: AppService) {
-  }
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
 
-  ngOnInit() {
-    this.appService.testRoute().subscribe(data => this.msg = data['msg']);
-  }
+  ngOnInit() {}
+
+	select_round(){
+    console.log(this.track.id_track)
+    this.http.get<any>(this.BASE_URL).subscribe(data => {
+      this.seasonRoundData = data;
+      console.log(this.seasonRoundData)
+    })
+	}
 }
